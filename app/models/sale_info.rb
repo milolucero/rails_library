e751f5416b8b13a5edfa90dab9b93a90a3be1a0b
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-10-08 09:13:58 -0500

    Reimplement validation

--- app/models/sale_info.rb
+++ app/models/sale_info.rb
@@ -1,5 +1,5 @@
 class SaleInfo < ApplicationRecord
   has_one :book
 
-  # validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
