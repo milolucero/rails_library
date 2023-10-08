2161b4af6cc8b83c0e92c1bdb439bad0c19229ba
Olena Reikh <olena.reikh@gmail.com>
2023-10-07 23:29:43 -0500

    Modify username with allowing period(dot)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   # Validation for username format (alphanumeric characters, underscores, and dashes)
   validates :username,
-            format: { with:    /\A[a-zA-Z0-9_-]+\z/,
+            format: { with:    /\A[a-zA-Z0-9_.-]+\z/,
                       message: " Username can only contain letters, numbers, underscores, and dashes" }
 
   # Validation for email format
@@ -18,14 +18,4 @@ class User < ApplicationRecord
   # Validation for length (assuming minimum and maximum lengths)
   validates :username, length: { minimum: 3, maximum: 30 }
   validates :first_name, :last_name, length: { maximum: 50 }
-
-  # Custom validation example (you can define your own validation logic)
-  validate :username_cannot_contain_special_characters
-
-  # Custom validation method
-  def username_cannot_contain_special_characters
-    return unless username.present? && username.match(/[^a-zA-Z0-9_-]/)
-
-    errors.add(:username, "Can only contain letters, numbers, underscores, and dashes")
-  end
 end
