882e1e7019c05285fd1ca097be0382114e02b89f
Olena Reikh <olena.reikh@gmail.com>
2023-10-10 21:07:52 -0500

    Add content and styles for About page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,4 +1,7 @@
+require "json"
 class AboutController < ApplicationController
   def index
+    json_data = File.read(Rails.root.join("db/dataset_description.json"))
+    @data = JSON.parse(json_data)
   end
 end
