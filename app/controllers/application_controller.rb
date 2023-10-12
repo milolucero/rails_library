7d7379cd2b108028b5b6d5981ebd21415255fc02
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-10-12 02:05:09 -0500

    Add top navigation placeholder with book categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_categories
+
+  private
+
+  def set_categories
+    # @categories includes all the columns of the categories table, plus a "book_count" column.
+    @categories = Category.joins(:books).group("categories.id").select("categories.*, COUNT(books.id) AS book_count").order(:name)
+  end
 end
