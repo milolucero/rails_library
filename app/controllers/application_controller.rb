292ad90cee02464f96eef2fb3a008137b63a4063
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-10-16 10:08:23 -0500

    Display publisher on search results message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :set_categories
+  before_action :set_variables
+
 
   private
 
-  def set_categories
+  def set_variables
     # @categories includes all the columns of the categories table, plus a "book_count" column.
     @categories = Category.joins(:books).group("categories.id").select("categories.*, COUNT(books.id) AS book_count").order(:name)
+    @publishers = Publisher.all
   end
 end
