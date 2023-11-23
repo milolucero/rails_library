a091277f2d69c8f2b6287561c6aaf9653b88d5a1
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-11-23 12:50:31 -0600

    Implement cart subtotal price calculation

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,9 @@
 class CartController < ApplicationController
   def index
-    @cart_total = 100
+    @cart_total = @cart.sum do |item|
+      book = Book.find(item["id"])
+      unit_price = book.is_on_sale ? book.sale_price : book.price
+      item["quantity"] * unit_price
+    end
   end
 end
