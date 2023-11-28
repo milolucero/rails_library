96646b10601a66da2a734688a4f0c84f1d3f824c
Olena Reikh <olena.reikh@gmail.com>
2023-11-27 20:13:53 -0600

    Add devise to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
   belongs_to :province
   has_many :reviews
   has_many :books, through: :reviews
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'pages/show'
   get 'cart/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
