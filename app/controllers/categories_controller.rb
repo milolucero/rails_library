500a9a7677ac7ce124b353bcbcb464a6435c5729
Olena Reikh <olena.reikh@gmail.com>
2023-12-04 20:07:38 -0600

    Add Filters into left side pane

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,5 +7,34 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by(name: params[:category_name])
     @books = @category.books
+    apply_filters
   end
+
+  def apply_filters
+    filter_conditions = []
+
+    if params[:on_sale].present?
+      filter_conditions << { is_on_sale: true }
+    end
+
+    if params[:new].present?
+      filter_conditions << ['books.created_at >= ?', 3.days.ago]
+    end
+
+    if params[:recently_updated].present?
+      # Filter based on recently updated, excluding new products
+      filter_conditions << ['books.updated_at >= ? AND books.created_at < ?', 3.days.ago, 3.days.ago]
+    end
+
+
+    # Apply all filter conditions to @books
+    filter_conditions.each do |condition|
+      if condition.is_a?(Hash)
+        @books = @books.where(condition)
+      else
+        @books = @books.where(*condition)
+      end
+    end
+  end
+
 end
