2b7972eaf0da4098a2a207df8b24da6b438852b1
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-12-10 00:22:20 -0600

    Fix bug on shipping address form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update_columns(user_params.to_h)
-      redirect_to @user, notice: 'Address was successfully updated.'
+      redirect_back(fallback_location: root_path, notice: 'Address was successfully updated.')
     else
       render :edit
     end
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    # params.require(:user).permit(:address, :city, :province, :postal_code)
     params.require(:user).permit(:address, :city, :province_id, :postal_code)
   end
 end
